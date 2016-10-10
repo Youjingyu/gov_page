aaad0cad0617a4a0b32c6db13e9ac02e834125d0
whale <735284268@qq.com>
2016-10-10 22:57:10 +0800

    add css

--- js/init.js
+++ js/init.js
@@ -1,21 +1,4 @@
 $(document).ready(function() {
-    (function (doc, win) {
-        var docEl = doc.documentElement,
-            //resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
-            recalc = function () {
-                var clientWidth = docEl.clientWidth;
-                if (!clientWidth) return;
-                docEl.style.fontSize = 12.5 * (clientWidth / 1200) + 'px';
-            };
-        if (doc.addEventListener){
-            win.addEventListener('resize', recalc, false);
-            //doc.addEventListener('DOMContentLoaded', recalc, false);
-        } else{
-            win.attachEvent('onresize', recalc);
-            //doc.attachEvent('onDOMContentLoaded', recalc);
-        }
-        recalc();
-    })(document, window);
     $('#map').height($('#map').width());
     $('#calendar').height($('#calendar').width());
     var calendar_width = $('#calendar').width();
