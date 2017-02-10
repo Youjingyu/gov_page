a692bbd691e95e840b4b59ae5046fb65ba240097
Youjingyu <735284268@qq.com>
2017-02-10 14:13:07 +0800

    simplified switchMonthToEn function

--- js/calendar.js
+++ js/calendar.js
@@ -141,44 +141,8 @@ $(document).ready(function() {
     }
 
     function switchMonthToEn(month) {
-        switch (parseInt(month)) {
-            case 0:
-                return '1';
-                break;
-            case 1:
-                return '2';
-                break;
-            case 2:
-                return '3';
-                break;
-            case 3:
-                return '4';
-                break;
-            case 4:
-                return '5';
-                break;
-            case 5:
-                return '6';
-                break;
-            case 6:
-                return '7';
-                break;
-            case 7:
-                return '8';
-                break;
-            case 8:
-                return '9';
-                break;
-            case 9:
-                return '10';
-                break;
-            case 10:
-                return '11';
-                break;
-            case 11:
-                return '12';
-                break;
-        }
+        var month_arr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
+        return month_arr[month];
     }
 
     function switchLevelToCss(level){
